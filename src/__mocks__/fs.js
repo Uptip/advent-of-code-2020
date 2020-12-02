ec91466bf742875c01cddfdb5cd5e15ba26d4f28
Henri Michel <henri@tymate.com>
2020-12-02 12:08:22 +0100

    test: test readFile util function

---
+++ src/__mocks__/fs.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const fs = jest.createMockFromModule('fs');
+
+let mockFiles = {};
+const __setMockFiles = newMockFiles => {
+  mockFiles = {};
+  for (const file in newMockFiles) {
+    mockFiles[file] = newMockFiles[file];
+  }
+};
+
+const readFile = pathToFile => {
+  console.log(pathToFile);
+  return mockFiles[pathToFile];
+};
+
+fs.__setMockFiles = __setMockFiles;
+fs.promises = { readFile };
+
+module.exports = fs;
